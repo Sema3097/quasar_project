1ba583075925c0d878ffb075b59bb6900a4de952
Kanishchev Semyon <k.sema3001k.sema@gmail.com>
2024-08-25 14:36:03 +0700

    first

---
+++ src/pages/FirstPage.vue
@@ -0,0 +1,75 @@
+<template>
+  <div class="q-pa-md" style="max-width: 400px; margin: 0 auto">
+    <q-form @submit.prevent class="q-gutter-md">
+      <q-input v-model="dataToSend.name" label="Name" />
+      <q-input v-model="dataToSend.surname" label="Surname" />
+      <q-select v-model="dataToSend.city" :options="options" label="City" />
+      <q-input v-model="dataToSend.email" label="Email" />
+      <q-input v-model="dataToSend.date" filled type="date" hint="Date"/>
+      <div>
+        <q-btn label="Save" type="submit" color="primary" @click="postData" />
+      </div>
+    </q-form>
+  </div>
+</template>
+
+<script setup>
+defineOptions({
+  name: "FirstPage",
+});
+</script>
+
+<script>
+import { uid } from "quasar";
+import axios from "axios";
+
+export default {
+  data() {
+    return {
+      dataToSend: {
+        name: "",
+        surname: "",
+        city: "",
+        email: "",
+        date: "",
+      },
+      options: [
+        "Moscow",
+        "Saint Petersburg",
+        "Kazan",
+        "Yekaterinburg",
+        "Novosibirsk",
+      ],
+    };
+  },
+
+  methods: {
+    postData() {
+      const newData = {
+        id: uid(),
+        name: this.dataToSend.name,
+        surname: this.dataToSend.surname,
+        city: this.dataToSend.city,
+        email: this.dataToSend.email,
+        date: this.dataToSend.date,
+      };
+      this.dataToSend.name = "";
+      this.dataToSend.surname = "";
+      this.dataToSend.city = "";
+      this.dataToSend.email = "";
+      this.dataToSend.date = "";
+
+      axios
+        .post("http://localhost:9000/", {
+          newData,
+        })
+        .then(function (res) {
+          console.log(res);
+        })
+        .catch(function (error) {
+          console.error(error);
+        });
+    },
+  },
+};
+</script>
---
+++ src/pages/SecondPage.vue
@@ -0,0 +1,27 @@
+<template>
+  <q-page class="flex flex-center">
+    <main class="text-weight-medium" style="padding: 0 100px">
+      <ol style="display: flex; flex-direction: column; gap: 20px">
+        <li>Поднять проект на VUE, подключить quasar;</li>
+        <li>
+          Главная страница - приветствие, в шапке ниспадающее меню (переход на 2
+          страницы + на главную);
+        </li>
+        <li>
+          Страница1 - форма из минимум 3х полей(дата, текст, select), данные из
+          которой отправляются на сервер при нажатии кнопки сохранить (post
+          запрос через axios, можно просто на localhost, ответ не нужен);
+        </li>
+        <li>Страница2 - Текст тестового задания.</li>
+      </ol>
+    </main>
+  </q-page>
+</template>
+
+<script setup>
+defineOptions({
+  name: "SecondPage",
+});
+</script>
+
+<style></style>
